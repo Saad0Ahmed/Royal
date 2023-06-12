5eb4f4e93cd222735dca7208863ddec6ed5df60c
Omen_op <96862317+Omenop@users.noreply.github.com>
2023-06-12 12:02:33 +0530

    Update guildMemberAdd.js

--- src/Events/Guild/guildMemberAdd.js
+++ src/Events/Guild/guildMemberAdd.js
@@ -8,41 +8,41 @@ const WelcomeCard = require("../../Utils/WelcomeCard");
  * @return {Promise<void>}
  */
 const execute = async (member, interaction) => {
-    if (member.user.bot) return;
-    const welcomeDocument = await welcomeSchema.findOne({
-        guildId: member.guild.id,
-    });
-    if (!welcomeDocument) return;
-    // Get the welcome channel and check if it's a text channel
-    const welcomeChannel = member.guild.channels.cache.get(
-        welcomeDocument.channelId
-    );
-    if (!welcomeChannel || !welcomeChannel.isTextBased()) return;
-
+  if (member.user.bot) return;
+  const welcomeDocument = await welcomeSchema.findOne({
+    guildId: member.guild.id,
+  });
+  if (!welcomeDocument) return;
+  // Get the welcome channel and check if it's a text channel
+  const welcomeChannel = member.guild.channels.cache.get(
+    welcomeDocument.channelId
+  );
+  if (!welcomeChannel || !welcomeChannel.isTextBased) {
     // Create a new WelcomeCard and set its properties
     const welcomeCard = new WelcomeCard();
     welcomeCard
-        .setAvatarUrl(member.user.avatarURL({ extension: "png", size: 1024 }))
-        .setMemberCount(member.guild.memberCount)
-        .setTag(member.user.tag);
+      .setAvatarUrl(member.user.avatarURL({ extension: "png", size: 1024 }))
+      .setMemberCount(member.guild.memberCount)
+      .setTag(member.user.tag);
 
     // Draw the welcome image and create a new Discord attachment from the buffer
     const attachment = await welcomeCard.draw();
 
     try {
-        // Send the welcome message with the image attachment
-        welcomeChannel.send({
-            content: `:wave: Hello ${member}, Welcome to ${member.guild.name}`,
-            files: [attachment],
-        });
-        if (welcomeDocument.roleId && member.guild.roles.cache.has(welcomeDocument.roleId)) {
-            member.roles.add(welcomeDocument.roleId);
-        }
+      // Send the welcome message with the image attachment
+      welcomeChannel.send({
+        content: `:wave: Hello ${member}, Welcome to ${member.guild.name}`,
+        files: [attachment],
+      });
+      if (welcomeDocument.roleId && member.guild.roles.cache.has(welcomeDocument.roleId)) {
+        member.roles.add(welcomeDocument.roleId);
+      }
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
+  }
 };
 module.exports = {
-    name: "guildMemberAdd",
-    execute
+  name: "guildMemberAdd",
+  execute,
 };
